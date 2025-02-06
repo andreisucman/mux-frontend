16de1d8e0e166e469362c7d1d3dc5338baea11a6
Andrei <sucmanandrei@gmail.com>
2025-02-06 14:01:37 +0300

    bug fixing

--- components/AnalysisCarousel/BetterThanCardPotential/index.tsx
+++ components/AnalysisCarousel/BetterThanCardPotential/index.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React, { useMemo } from "react";
-import { Skeleton, Stack, Text, Title } from "@mantine/core";
+import { Skeleton, Stack, Text } from "@mantine/core";
 import { useElementSize } from "@mantine/hooks";
 import { AuthStateEnum } from "@/context/UserContext/types";
-import getAppearanceVerdict from "@/helpers/getAppearanceVerdict";
 import { getRingColor } from "@/helpers/utils";
 import { FormattedRatingType, TypeEnum } from "@/types/global";
 import ComparisonStack from "../BetterThanCard/ComparisonStack";
@@ -61,20 +60,12 @@ function BetterThanCardPotential({
     return size;
   }, [containerHeight]);
 
-  const verdictTitle = useMemo(
-    () => getAppearanceVerdict(potentialRecord.overall as number, true),
-    [potentialRecord.overall]
-  );
-
   return (
     <Skeleton className="skeleton" visible={containerHeight === 0}>
       <Stack className={classes.container} ref={ref}>
         <Text className={classes.title} c="dimmed">
           {title}
         </Text>
-        <Title order={2} ta="center">
-          {verdictTitle}
-        </Title>
         {partCircleObjects.length > 1 && (
           <ComparisonStack
             higherThanNumber={percentage}
